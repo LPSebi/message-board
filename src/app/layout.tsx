16a7449cbbb238322d971567b98578809a4ff585
Fl0w <52174534+LPSebi@users.noreply.github.com>
2024-01-30 00:16:45 +0100

    Refactor layout component and add providers module

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,28 @@
-import { Inter } from "next/font/google";
-import { ThemeProvider } from "~/components/theme-provider";
-import "~/styles/globals.css";
+import { Inter } from "next/font/google"
+import Providers from "~/lib/providers"
+import "~/styles/globals.css"
 
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+    subsets: ["latin"],
+    variable: "--font-sans",
+})
 
 export const metadata = {
-  title: "Temporary Message Board",
-  description: "A message board that resets every week.",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
-};
+    title: "Temporary Message Board",
+    description: "A message board that resets every week.",
+    icons: [{ rel: "icon", url: "/favicon.ico" }],
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`font-sans ${inter.variable}`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={`font-sans ${inter.variable}`}>
+                <Providers>{children}</Providers>
+            </body>
+        </html>
+    )
 }
