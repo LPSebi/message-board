56697270169a82cd1b0cc73695a42f63ed8eb9b2
Fl0w <52174534+LPSebi@users.noreply.github.com>
2024-02-11 23:26:05 +0100

    Add @vercel/speed-insights package

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -4,6 +4,7 @@ import { SessionProvider } from "next-auth/react"
 import { ThemeProvider } from "~/components/theme-provider"
 import Toaster from "~/components/ui-custom/toaster"
 import { SocketProvider } from "~/lib/socketio/provider"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     return (
@@ -16,6 +17,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
             <SessionProvider>
                 <SocketProvider>{children}</SocketProvider>
                 <Toaster />
+                <SpeedInsights />
             </SessionProvider>
         </ThemeProvider>
     )
