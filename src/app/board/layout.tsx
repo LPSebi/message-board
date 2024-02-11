0cfe9ab7d8191b02c1db21352ba2d5e154972813
Fl0w <52174534+LPSebi@users.noreply.github.com>
2024-02-11 21:00:44 +0100

    Update layout dimensions

--- src/app/board/layout.tsx
+++ src/app/board/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <main className="flex h-screen w-screen items-center justify-center bg-background py-5">
+        <main className="flex h-[100dvh] w-[100dvw] items-center justify-center bg-background py-5">
             {children}
         </main>
     )
