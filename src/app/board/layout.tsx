27ac59535cb50857d7e143b6753f6f1ffafa3acc
Fl0w <52174534+LPSebi@users.noreply.github.com>
2024-02-16 20:04:21 +0100

    Update layout and page styles

--- src/app/board/layout.tsx
+++ src/app/board/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <main className=" bg-dot-white/[0.1] flex h-[100dvh] w-[100dvw] items-center justify-center bg-background py-5">
+        <main className="flex h-[100dvh] w-[100dvw] items-center justify-center bg-background py-5 bg-dot-black/[0.2] dark:bg-dot-white/[0.1]">
             {children}
         </main>
     )
